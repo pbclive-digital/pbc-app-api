4a6b5e04aca46c181138f0c35d8dbc585e7f693e
Kavimal Wijewardana <kavimalw@gmail.com>
2025-08-06 20:02:53 -0400

    Update deploy.sh script for run the application locally.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 STAGING_FILE=module-remote-datastore/src/main/resources/firebase/pbc-live-service-account-key-staging.json
 PROD_FILE=module-remote-datastore/src/main/resources/firebase/pbc-live-service-account-key-prod.json
 
+SECRETS_REPO=pbc-app-secrets/
 STAGING_SECRET=pbc-app-secrets/firebase-service-account-secrets/pbc-live-service-account-key-staging.json
 PROD_SECRET=pbc-app-secrets/firebase-service-account-secrets/pbc-live-service-account-key-prod.json
 
@@ -10,6 +11,14 @@ SECRETS_PATH=module-remote-datastore/src/main/resources/firebase/
 
 function clone_secrets() {
     git clone git@github.com:pbclive-digital/pbc-app-secrets.git
+
+    if [ -d $SECRETS_REPO ]; then
+      echo "Clone the secrets successfully."
+      return 0
+    else
+      echo "This environment don't have permission to clone secrets."
+      return 1
+    fi
 }
 
 function check_staging_file_existence() {
@@ -33,21 +42,36 @@ function delete_clone_secrets() {
 }
 
 function move_secrets() {
-    if [[ $env == "staging" ]]; then
-      if check_staging_file_existence; then
-        echo "service account key already available"
-      else
-        cp $STAGING_SECRET $SECRETS_PATH
-      fi
-    else
-      if [[ $env == "prod" ]]; then
+    case $env in
+      "staging"|"local")
+        if check_staging_file_existence; then
+          echo "service account key already available"
+        else
+          cp $STAGING_SECRET $SECRETS_PATH
+        fi
+        ;;
+      "prod")
         if check_prod_file_existence; then
           echo "service account key already available"
         else
           cp $PROD_SECRET $SECRETS_PATH
         fi
-      fi
-    fi
+        ;;
+      *)
+        echo "Not valid environment provided."
+        ;;
+    esac
+}
+
+function deploy_execution() {
+    case $env in
+      "local")
+        ./gradlew clean --no-build-cache bootRun
+        ;;
+      *)
+        echo "Coming Soon"
+        ;;
+    esac
 }
 
 ## Referring static arguments
@@ -55,6 +79,10 @@ args=()
 env=$1
 
 ## Execute
-clone_secrets
-move_secrets $env
-delete_clone_secrets
+if clone_secrets; then
+  move_secrets
+  delete_clone_secrets
+  deploy_execution
+else
+  echo "Can not continue this operation without application secrets."
+fi
