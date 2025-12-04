1b141be66fc5080192c46fb2c4fa80571dcd08ad
Kavimal Wijewardana <kavimalw@gmail.com>
2025-12-04 16:11:35 -0500

    Introduce new end-point to manage news

--- pbc-api/src/main/kotlin/com/kavi/pbc/live/api/controller/NewsController.kt
+++ pbc-api/src/main/kotlin/com/kavi/pbc/live/api/controller/NewsController.kt
@@ -7,7 +7,11 @@ import com.kavi.pbc.live.data.model.news.News
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -25,6 +29,52 @@ class NewsController(private val newsService: NewsService) {
         logger.printInfo("REQUEST MAPPING: POST: [/news/create]", NewsController::class.java)
 
         val response = newsService.createNews(news)
+        logger.printResponseInfo(response, NewsController::class.java)
+
+        return response
+    }
+
+    @GetMapping("/get/draft")
+    fun getAllDraftNews(): ResponseEntity<BaseResponse<List<News>>>?? {
+        logger.printSeparator()
+        logger.printInfo("REQUEST MAPPING: GET: [/news/get/draft]", NewsController::class.java)
+
+        val response = newsService.getDraftNews()
+        logger.printResponseInfo(response, NewsController::class.java)
+
+        return response
+    }
+
+    @GetMapping("/get/active")
+    fun getAllActiveNews(): ResponseEntity<BaseResponse<List<News>>>? {
+        logger.printSeparator()
+        logger.printInfo("REQUEST MAPPING: GET: [/news/get/active]", NewsController::class.java)
+
+        val response = newsService.getActiveNews()
+        logger.printResponseInfo(response, NewsController::class.java)
+
+        return response
+    }
+
+    @PutMapping("/update/{news-id}")
+    fun updateNews(@PathVariable(value = "news-id") newsId: String,
+                    @Valid @RequestBody news: News): ResponseEntity<BaseResponse<News>>? {
+        logger.printSeparator()
+        logger.printInfo("REQUEST MAPPING: PUT: [/news/update/$newsId]", NewsController::class.java)
+
+        val response = newsService.updateNews(newsId, news)
+        logger.printResponseInfo(response, NewsController::class.java)
+
+        return response
+    }
+
+    @DeleteMapping("/delete/{news-id}")
+    fun deleteEvent(@PathVariable(value = "news-id") newsId: String):
+            ResponseEntity<BaseResponse<String>>? {
+        logger.printSeparator()
+        logger.printInfo("REQUEST MAPPING: DELETE:[/news/delete/$newsId]", NewsController::class.java)
+
+        val response = newsService.deleteGivenNews(newsId)
         logger.printResponseInfo(response, NewsController::class.java)
 
         return response
--- pbc-api/src/main/kotlin/com/kavi/pbc/live/api/service/NewsService.kt
+++ pbc-api/src/main/kotlin/com/kavi/pbc/live/api/service/NewsService.kt
@@ -1,11 +1,13 @@
 package com.kavi.pbc.live.api.service
 
 import com.kavi.pbc.live.api.dto.BaseResponse
+import com.kavi.pbc.live.api.dto.Error
 import com.kavi.pbc.live.api.dto.Status
 import com.kavi.pbc.live.com.kavi.pbc.live.integration.DatastoreRepositoryContract
 import com.kavi.pbc.live.com.kavi.pbc.live.integration.firebase.datastore.DatastoreConstant
 import com.kavi.pbc.live.com.kavi.pbc.live.integration.firebase.datastore.FirebaseDatastoreRepository
 import com.kavi.pbc.live.data.model.news.News
+import com.kavi.pbc.live.data.model.news.NewsStatus
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -20,5 +22,77 @@ class NewsService {
             .status(HttpStatus.CREATED)
             .body(BaseResponse(Status.SUCCESS,
                 datastoreRepositoryContract.createEntity(DatastoreConstant.NEWS_COLLECTION, news.id, news), null))
+    }
+
+    fun getDraftNews(): ResponseEntity<BaseResponse<List<News>>>? {
+        val properties = mapOf(
+            "newsStatus" to NewsStatus.DRAFT
+        )
+        val orderBy = mapOf(
+            "property" to "createdTime",
+            "direction" to "ASC"
+        )
+
+        val finalDraftNewsList = datastoreRepositoryContract.getEntityListFromProperties(
+            entityCollection = DatastoreConstant.NEWS_COLLECTION,
+            propertiesMap = properties,
+            orderByMap = orderBy,
+            className = News::class.java
+        )
+
+        return if (finalDraftNewsList.isNotEmpty()) {
+            ResponseEntity.ok(BaseResponse(Status.SUCCESS, finalDraftNewsList, null))
+        } else {
+            ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(BaseResponse(Status.ERROR, null, listOf(
+                    Error(HttpStatus.NOT_FOUND.toString()))
+                ))
+        }
+    }
+
+    fun getActiveNews(): ResponseEntity<BaseResponse<List<News>>>? {
+        val properties = mapOf(
+            "newsStatus" to NewsStatus.ACTIVE
+        )
+        val orderBy = mapOf(
+            "property" to "createdTime",
+            "direction" to "ASC"
+        )
+
+        val finalActiveNewsList = datastoreRepositoryContract.getEntityListFromProperties(
+            entityCollection = DatastoreConstant.NEWS_COLLECTION,
+            propertiesMap = properties,
+            orderByMap = orderBy,
+            className = News::class.java
+        )
+
+        return if (finalActiveNewsList.isNotEmpty()) {
+            ResponseEntity.ok(BaseResponse(Status.SUCCESS, finalActiveNewsList, null))
+        } else {
+            ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(BaseResponse(Status.ERROR, null, listOf(
+                    Error(HttpStatus.NOT_FOUND.toString()))
+                ))
+        }
+    }
+
+    fun updateNews(newsId: String, news: News): ResponseEntity<BaseResponse<News>> {
+        datastoreRepositoryContract.updateEntity(DatastoreConstant.NEWS_COLLECTION,
+            newsId, news)
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(BaseResponse(Status.SUCCESS,
+                news, null))
+    }
+
+    fun deleteGivenNews(newsId: String): ResponseEntity<BaseResponse<String>>? {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(BaseResponse(Status.SUCCESS,
+                datastoreRepositoryContract.deleteEntity(DatastoreConstant.NEWS_COLLECTION, newsId),
+                null))
     }
 }
