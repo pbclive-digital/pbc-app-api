0ccbc1a8c2913648cdca2955a7b1b71e752ba6f3
Kavimal Wijewardana <kavimalw@gmail.com>
2026-01-27 23:04:25 -0500

    Created a @Bean instead of WebMvcConfigurer as Gemini suggest

--- pbc-api/src/main/kotlin/com/kavi/pbc/live/api/security/SecurityConfiguration.kt
+++ pbc-api/src/main/kotlin/com/kavi/pbc/live/api/security/SecurityConfiguration.kt
@@ -1,17 +1,27 @@
 package com.kavi.pbc.live.api.security
 
+import com.kavi.pbc.live.api.AppProperties
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfiguration {
 
+    @Autowired
+    lateinit var appProperties: AppProperties
+
     @Bean
     fun securityFilterChain(http: HttpSecurity, tokenFilter: AppTokenFilter): SecurityFilterChain {
         http
+            .cors(Customizer.withDefaults())
             .csrf { it.disable() }
             .authorizeHttpRequests { requests ->
                 requests
@@ -44,5 +54,25 @@ class SecurityConfiguration {
     @Bean
     fun tokenFilter(): AppTokenFilter {
         return AppTokenFilter()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val allowedOriginList = when(appProperties.appEnv) {
+            "dev", "staging" -> listOf("http://localhost:8080", "https://pbclive-digital.github.io")
+            else -> emptyList()
+        }
+
+        val configuration = CorsConfiguration()
+        configuration.apply {
+            allowedOrigins = allowedOriginList
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
