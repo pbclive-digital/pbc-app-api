da213eed30e1f640fd7530171c7d7a8e02d1b798
Kavimal Wijewardana <kavimalw@gmail.com>
2026-01-27 20:27:13 -0500

    Remove unwanted imports

--- pbc-api/src/main/kotlin/com/kavi/pbc/live/api/security/SecurityConfiguration.kt
+++ pbc-api/src/main/kotlin/com/kavi/pbc/live/api/security/SecurityConfiguration.kt
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-import org.springframework.security.web.firewall.HttpFirewall
-import org.springframework.security.web.firewall.StrictHttpFirewall
 
 @Configuration
 class SecurityConfiguration {
